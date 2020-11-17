686f9a13db4c1031ebe5de1323957e05e8573aec
mat-planchot <mat_planchot@hotmail.fr>
2020-11-17 22:47:28 +0100

    init

---
+++ app/src/main/java/fr/epsi/gomi/ConnexionActivity.java
@@ -0,0 +1,13 @@
+package fr.epsi.gomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
