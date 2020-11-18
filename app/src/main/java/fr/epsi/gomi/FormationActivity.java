dc0946d0fa155c9fa96c713fa423eab0ac823c03
CLEMENT Yoann <yoann.clement@hotmail.fr>
2020-11-18 19:40:14 +0100

    Page formation + Localisation + Paramètres

---
+++ app/src/main/java/fr/epsi/gomi/FormationActivity.java
@@ -0,0 +1,13 @@
+package fr.epsi.gomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formation);
+    }
+}
---
+++ app/src/main/java/fr/epsi/gomi/LocalisationActivity.java
@@ -0,0 +1,14 @@
+package fr.epsi.gomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localisation);
+    }
+}
