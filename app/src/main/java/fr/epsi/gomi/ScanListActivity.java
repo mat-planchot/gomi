a58d289e72fc229b4f9a4e5d37c1761f58e2d775
CLEMENT Yoann <yoann.clement@hotmail.fr>
2020-11-19 12:27:01 +0100

    Boutons page settings + modification

--- app/src/main/java/fr/epsi/gomi/ScanListActivity.java
+++ app/src/main/java/fr/epsi/gomi/ScanListActivity.java
@@ -1,14 +1,14 @@
 package fr.epsi.gomi;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
